4f97d8a896079f4bee961e0a58b04a02701cafc9
Md. Majharul Islam <majharul.islam03@northsouth.edu>
2024-07-15 02:29:43 +0600

    widgets

---
+++ lib/views/shared_preference.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SharedPreference extends StatefulWidget {
+  const SharedPreference({super.key});
+
+  @override
+  State<SharedPreference> createState() => _SharedPreferenceState();
+}
+
+class _SharedPreferenceState extends State<SharedPreference> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
